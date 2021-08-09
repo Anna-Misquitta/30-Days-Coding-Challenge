599135717936faafb8e191cb91c8ef13bab11b0d
anna <misquittaanna@gmail.com>
2021-08-09 18:49:45 +0530

    Hello Techgig

---
+++ Day_1.php
@@ -0,0 +1,8 @@
+<?php
+  /* Read input from STDIN. Print your output to STDOUT*/
+  $fp = fopen("php://stdin", "r");
+  //Write code here
+  $input = readline();
+  echo "Hello Techgig\n";
+  echo $input;
+?>

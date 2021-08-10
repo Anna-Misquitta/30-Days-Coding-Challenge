30eaa3305e73a71b1345edd3e9949f78ed10d064
anna <misquittaanna@gmail.com>
2021-08-10 14:28:04 +0530

    Is Your Number Narcissistic?

---
+++ Day_9.php
@@ -0,0 +1,16 @@
+<?php
+  /* Read input from STDIN. Print your output to STDOUT*/
+    $__fp = fopen("php://stdin", "r");
+  //Write code here
+  $input = readline();
+  $length = strlen($input);
+  $num = 0;
+  for($i = 0; $i < $length; $i++){
+    $num += pow($input[$i],$length);
+  }
+  if($num == $input){
+    echo "True";
+  }else{
+    echo "False";
+  }
+?>
